a5845e9c73f18427991e84ba104ba8b806e925bc
nkhakwan <post_khan@yahoo.com>
2020-04-09 11:46:54 -0700

    add initial html for dice display

--- js/scripts.js
+++ js/scripts.js
@@ -59,6 +59,13 @@ var endOfGame = function() {
   $(".gameover").show();
   $(".game").hide();
   playerResults.round = 0;
+  if (playerResults.player1GrandTotal === playerResults.player2GrandTotal) {
+    $(".winner").html("Tie!");
+  } else if (playerResults.player1GrandTotal > playerResults.player2GrandTotal){
+    $(".winner").html("Player 1!");
+  } else {
+    $(".winner").html("Player 2!");
+  }
 }
 
 var refreshScores = function() {
